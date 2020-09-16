30fb124e294c6043716da1250148f39643ed9606
Nithin Kumar <nithinbemitk@gmail.com>
2020-09-16 09:52:35 +0530

    Added basic dashboard design

--- ccms/src/App.js
+++ ccms/src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import notificatioIcon from '../src/Images/NotificatioIcon.png'
+import profileIcon from '../src/Images/profile.png'
+import logo from '../src/Images/AppLogo.png'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='Parent'>
+      <div className='LeftPanel'>
+        <h4>Nav Items</h4>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </div>
+
+      <div className='RightPanel'>
+        <div className='RightHeader'>
+          <div className='HeaderText'>
+            <h5>Dashboard</h5>
+          </div>
+          <div className='HeaderNotification'>
+            <img src={notificatioIcon} />
+          </div>
+          <div className='HeaderProfile'>
+          <img src={profileIcon} />
+          </div>
+
+        </div>
+        <div className='RightContent'>
+          <h1>content</h1>
+        </div>
+      </div>
     </div>
   );
 }
