e3a839e181d3919c53be05fb2f5c1139def44ae6
Nithin Kumar <nithinbemitk@gmail.com>
2020-09-17 11:21:43 +0530

    Added Life cycle to Login page.

--- ccms/src/components/login/Login.js
+++ ccms/src/components/login/Login.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../../images/LoginBackground.png'
 import icon from '../../images/MicrosoftIcon.png'
 import { useDispatch } from 'react-redux'
- import { loggedIn } from '../../actions'
+import { loggedIn } from '../../actions'
 
 
 function Login() {
+
+    const [signIn, setSignIn] = useState("Log In");
 
     const dispatch = useDispatch();
 
@@ -15,11 +17,26 @@ function Login() {
 
     }
 
+    // // Similar to componentDidMount and componentDidUpdate:
+   
+    useEffect(
+        () => {
+            setSignIn("Sign In")
+          
+            //To clean up, Return a callback in useEffect and it will be called before unmounting
+          return () => {
+            setSignIn("Log Out")
+          };
+        },
+        [] // Pass an empty array to run only callback on mount only.
+      );
+
+
 
     return (
         <div className="loginCss">
             <div className="loginLeftCss">
-                <h3>Sign In</h3>
+                <h3>{signIn}</h3>
                 <button ><img src={icon} onClick={LoginClicked}></img></button>
             </div>
             <div className="loginRightCss">
