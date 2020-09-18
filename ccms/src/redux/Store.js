933d24567fd182374cf66b47f2d60d03ed418539
Krishnaprasad <krishnaprasadnaravi@gmail.com>
2020-09-18 11:59:09 +0530

    File restucturing

--- ccms/src/redux/Store.js
+++ ccms/src/redux/Store.js
@@ -5,7 +5,7 @@ import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
 // root reducer
-import allReducer from '../reducers'
+import allReducer from './reducers'
 
 const config = {
     key: 'ccms',
