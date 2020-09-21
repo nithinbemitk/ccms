4b68b3e4145d4adf33ad58a5a1f673d132131eb4
Krishnaprasad <krishnaprasadnaravi@gmail.com>
2020-09-21 15:44:36 +0530

    setup the class structure for api calls. It uses a sample REST api server.

--- ccms/src/components/coaching/Coaching.js
+++ ccms/src/components/coaching/Coaching.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getPosts } from '../../service'
 
 function Coaching() {
+
+    useEffect(() => {
+        getPosts()
+            .then((respone) => {
+                console.log("*** getPosts - ", respone)
+            })
+            .catch((error) => {
+                console.log("*** getPosts error - ", error)
+            })
+    });
 
     return (
         <div >
---
+++ ccms/src/service/index.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import { BASE_URL, endPoints } from './config';
+import makeApiRequest from './request';
+
+/**
+ * this uses a promise, which makes a request, which internally uses axios 
+ */
+export const getUsers = () => {
+    return new Promise((resolve, reject) => {
+        const params = {}
+        const header = null
+        const options = { url: endPoints.users, method: 'GET', params, header }
+
+        makeApiRequest(options)
+            .then((response) => {
+                resolve(response)
+            })
+            .catch((error) => {
+                reject(error)
+            })
+    });
+
+}
+/**
+ * this returns a promise which uses axios directly
+ */
+export const getPosts = () => {
+    return new Promise((resolve, reject) => {
+        axios.get(`${BASE_URL + endPoints.posts}`)
+            .then(function (response) {
+                resolve(response)
+            })
+            .catch(function (error) {
+                reject(error)
+            })
+    });
+}
