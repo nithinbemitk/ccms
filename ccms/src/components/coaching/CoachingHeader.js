d7b770205f22e6f5e9a018914ed5bcb2ec371082
Krishnaprasad <krishnaprasadnaravi@gmail.com>
2020-09-28 17:59:29 +0530

    Radium is integrated for inline style

--- ccms/src/components/coaching/CoachingHeader.js
+++ ccms/src/components/coaching/CoachingHeader.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Coaching.css'
+import Radium from 'radium'
 import { Button } from '@material-ui/core';
 
 const CoachingHeader = (props) => {
@@ -8,21 +9,37 @@ const CoachingHeader = (props) => {
         props.displayPlan(true);
     }
 
-    const showHideNewPlan = () => {
-        if (!props.newPlan) {
-            return <button className='RightSide' onClick={showPlans}>New Plan</button>
-            //return <Button className='RightSide' variant="contained" color="primary" onClick={showPlans}>New Plan</Button>
-        } else {
-            return null
+    const rightSide = {
+        justifyContent: "right",
+        alignItems: "center",
+        marginLeft: "auto",
+        height: "40px",
+        marginTop: "20px",
+        marginRight: "20px",
+        backgroundColor: "aqua",
+        ":hover": {
+            backgroundColor: "blue",
+            color: "white"
         }
-    }
-    return (
-        <div className="CoachingHeader">
-            <h1 className="LeftSide"> Coaching Header </h1>
 
-            {showHideNewPlan()}
-        </div>
-    );
+
 }
 
-export default CoachingHeader
+const showHideNewPlan = () => {
+    if (!props.newPlan) {
+        return <button style={rightSide} onClick={showPlans}>New Plan</button>
+        //return <Button className='RightSide' variant="contained" color="primary" onClick={showPlans}>New Plan</Button>
+    } else {
+        return null
+    }
+}
+return (
+    <div className="CoachingHeader">
+        <h1 className="LeftSide"> Coaching Header </h1>
+
+        {showHideNewPlan()}
+    </div>
+);
+}
+
+export default Radium(CoachingHeader);
