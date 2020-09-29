2363f9512fd70ad7072323ed64aae5248ff53872
Krishnaprasad <krishnaprasadnaravi@gmail.com>
2020-09-29 10:03:00 +0530

    Updates

---
+++ precache-manifest.3495b9ba6d1cefae6866eca8d7731992.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bb8c4b55880c697b41ea8740077b484",
+    "url": "/ccms/index.html"
+  },
+  {
+    "revision": "3b77b48368823b8a5015",
+    "url": "/ccms/static/css/2.aa9c7ed0.chunk.css"
+  },
+  {
+    "revision": "09637ccf810d08bfa378",
+    "url": "/ccms/static/css/main.594c7fe8.chunk.css"
+  },
+  {
+    "revision": "3b77b48368823b8a5015",
+    "url": "/ccms/static/js/2.6dad08ee.chunk.js"
+  },
+  {
+    "revision": "8f7daff30ddd7684922e23ac87b23b47",
+    "url": "/ccms/static/js/2.6dad08ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09637ccf810d08bfa378",
+    "url": "/ccms/static/js/main.dd88a5e8.chunk.js"
+  },
+  {
+    "revision": "2ede3ab93ba90bbf6b6f",
+    "url": "/ccms/static/js/runtime-main.be6c48e5.js"
+  },
+  {
+    "revision": "23d310933b9fe7ca80f9bc4347529e67",
+    "url": "/ccms/static/media/LoginBackground.23d31093.png"
+  },
+  {
+    "revision": "bfacef72d7ed0a72b129f49380a3f672",
+    "url": "/ccms/static/media/MicrosoftIcon.bfacef72.png"
+  }
+]);
